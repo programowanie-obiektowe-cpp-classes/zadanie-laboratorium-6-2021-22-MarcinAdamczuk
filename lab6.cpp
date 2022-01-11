e649a5206a3dd773e813de9cd959e9c3abadc96a
Marcin Adamczuk <m.adamczuk@o2.pl>
2022-01-11 19:37:11 +0100

    zad2

--- lab6.cpp
+++ lab6.cpp
@@ -5,10 +5,12 @@ using namespace std;
 
 int main()
 {
-    vector v1 = make_random_vector(10, 0, 20);
+    vector v1 = make_random_vector(10, 0, 10);
     for(int e : v1)
         cout << e << " ";
     cout << endl;
+
+    cout << count(v1.begin(), v1.end(), 7) << endl;
 
     sort(v1.begin(), v1.end());
     for(int e : v1)
