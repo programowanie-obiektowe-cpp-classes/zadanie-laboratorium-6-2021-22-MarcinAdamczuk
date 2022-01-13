d1342fab2a311d1ed3854fcd983878a8c992a96a
Marcin Adamczuk <m.adamczuk@o2.pl>
2022-01-13 19:12:44 +0100

    test

--- foo.hpp
+++ foo.hpp
@@ -9,8 +9,12 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
-
-    // Twoja implementacja tutaj
-
+    std::for_each(people.begin(), people.end(), [](Human& a) { a.birthday(); });
+    std::transform(people.begin(), people.end(), ret_v.rbegin(), [](Human& a){
+        if(a.isMonster() == true)
+            return ('n');
+        else
+            return ('y');
+    });
     return ret_v;
 }
